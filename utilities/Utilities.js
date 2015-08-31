545cae9d85c65f81e391dfed75ae61d9976b08c0
timeislife <rferen2008@gmail.com>
2015-08-31 13:48:38 +0800

    updated.

--- utilities/Utilities.js
+++ utilities/Utilities.js
@@ -23,32 +23,32 @@ module.exports = function(models) {
 }
 
 function AuthenticateFromLoginToken(req, res, next) {
-		  var cookie = JSON.parse(req.cookies.logintoken);
+	  var cookie = JSON.parse(req.cookies.logintoken);
 
-		  Models.LoginToken.findOne({ email: cookie.email,
-							   series: cookie.series,
-							   token: cookie.token }, (function(err, token) {
-			if (!token) {
-			  res.redirect('/sessions/new');
-			  return;
-			}
+	  Models.LoginToken.findOne({ email: cookie.email,
+						   series: cookie.series,
+						   token: cookie.token }, (function(err, token) {
+		if (!token) {
+		  res.redirect('/sessions/new');
+		  return;
+		}
 
-			Models.User.findOne({ email: token.email }, function(err, user) {
-			  if (user) {
-				req.session.user_id = user.id;
-				req.currentUser = user;
+		Models.User.findOne({ email: token.email }, function(err, user) {
+		  if (user) {
+			req.session.user_id = user.id;
+			req.currentUser = user;
 
-				token.token = token.randomToken();
-				token.save(function() {
-				  res.cookie('logintoken', token.cookieValue, { expires: new Date(Date.now() + 2 * 604800000), path: '/' });
-				  next();
-				});
-			  } else {
-				res.redirect('/sessions/new');
-			  }
+			token.token = token.randomToken();
+			token.save(function() {
+			  res.cookie('logintoken', token.cookieValue, { expires: new Date(Date.now() + 2 * 604800000), path: '/' });
+			  next();
 			});
-		  }));
-    }
+		  } else {
+			res.redirect('/sessions/new');
+		  }
+		});
+	  }));
+}
 
 
 function LoadUser(req, res, next)
